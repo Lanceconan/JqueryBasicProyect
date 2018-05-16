b9eb15a7074d24c4b3932d89ba75802f2106fc4c
Lanceconan <conan_022@hotmail.com>
2018-05-16 18:01:34 -0400

    Reemplazar codigo html

--- js/crearHTML.js
+++ js/crearHTML.js
@@ -3,3 +3,21 @@ $("#boton3").click(function(event) {
 	$("#contenedor").html('<div id="c1">Adios!</div>');
 
 });
+
+$("#boton4").click(function(event) {
+	/*Agregar contenido contenido*/
+	$("#contenedor").append('<div id="c2">Adios!</div>');
+
+	/*Agregar codigo HTML antes de la caja c1 seleccionada*/
+	$("#c1").before('<h1>Título</h1>');
+
+    /*Agregar codigo HTML antes de la caja c1 seleccionada*/
+	$("#c1").after('<h3>Párrafo</h3>');
+
+	/*Agregar texto nuevo antes del texto ya escrito*/
+	$("#c1").prepend('Bienvenido ');
+
+	/*Reemplazar con texto nuevo el texto ya escrito*/
+	$("#c2").text('Un texto nuevo');
+	
+});
