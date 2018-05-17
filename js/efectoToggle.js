4815a6109140f171223dcecb14560e5c25cd6278
Lanceconan <conan_022@hotmail.com>
2018-05-17 10:55:31 -0400

    resumen de acciones de jquery

--- js/efectoToggle.js
+++ js/efectoToggle.js
@@ -1,3 +1,19 @@
 $("#boton5").click(function(event) {
 	$("#c3").toggle();
 });
+
+/*
+
+.click() - evento al click
+.css() - cambiar estilo css
+.attr() - seleccionar o crear atributo
+.val() - obtener valor
+.addClass() - agregar una clase
+.removeClass() - eliminar clase
+.html() - reemplazar código html
+.append() - agregar código html
+.before() - agregar cajas antes
+.after() - agregar cajas despues
+.toogle() - botón presionado/nopresionado timbre
+
+*/
